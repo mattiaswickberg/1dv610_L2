d328526d7a99b7762e0974ef248b735f1308462a
Mattias Wickberg <ad13113@ad.ssis.nu>
2018-09-18 12:38:53 +0200

    aded srver time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l") . ', the ' . date("d") . 'th of ' . date("F") . ' ' . date("Y") .', The time is ' . date("h:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
